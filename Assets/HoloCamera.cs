46e41379f056a19a0825bcc1b75e01031debf0e7
CarlHawley <carlhawley@gmail.com>
2022-10-10 14:24:23 -0500

    added multiple photo control

--- Assets/HoloCamera.cs
+++ Assets/HoloCamera.cs
@@ -1,6 +1,5 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using System.Collections;
 using System.Linq;
 using UnityEngine.Windows.WebCam;
 
@@ -8,15 +7,14 @@ public class HoloCamera : MonoBehaviour
 {
     PhotoCapture photoCaptureObject = null;
     Texture2D targetTexture = null;
-
-
+    Resolution cameraResolution;
+    float offset = 0.0f;
     // Use this for initialization
     void Start()
     {
-        Resolution cameraResolution = PhotoCapture.SupportedResolutions.OrderByDescending((res) => res.width * res.height).First();
         targetTexture = new Texture2D(cameraResolution.width, cameraResolution.height);
+        cameraResolution = PhotoCapture.SupportedResolutions.OrderByDescending((res) => res.width * res.height).First();
 
-        // Create a PhotoCapture object
         PhotoCapture.CreateAsync(false, delegate (PhotoCapture captureObject) {
             photoCaptureObject = captureObject;
             CameraParameters cameraParameters = new CameraParameters();
@@ -24,23 +22,14 @@ public class HoloCamera : MonoBehaviour
             cameraParameters.cameraResolutionWidth = cameraResolution.width;
             cameraParameters.cameraResolutionHeight = cameraResolution.height;
             cameraParameters.pixelFormat = CapturePixelFormat.BGRA32;
-
-            // Activate the camera
-            photoCaptureObject.StartPhotoModeAsync(cameraParameters, delegate (PhotoCapture.PhotoCaptureResult result) {
-                // Take a picture
-                photoCaptureObject.TakePhotoAsync(OnCapturedPhotoToMemory);
-            });
         });
+
     }
 
     public void TakePhoto()
     {
-        Debug.Log("take photo");
-        // Take a picture
-        Resolution cameraResolution = PhotoCapture.SupportedResolutions.OrderByDescending((res) => res.width * res.height).First();
         targetTexture = new Texture2D(cameraResolution.width, cameraResolution.height);
 
-        // Create a PhotoCapture object
         PhotoCapture.CreateAsync(false, delegate (PhotoCapture captureObject) {
             photoCaptureObject = captureObject;
             CameraParameters cameraParameters = new CameraParameters();
@@ -55,30 +44,29 @@ public class HoloCamera : MonoBehaviour
                 photoCaptureObject.TakePhotoAsync(OnCapturedPhotoToMemory);
             });
         });
-
     }
-        void OnCapturedPhotoToMemory(PhotoCapture.PhotoCaptureResult result, PhotoCaptureFrame photoCaptureFrame)
+    void OnCapturedPhotoToMemory(PhotoCapture.PhotoCaptureResult result, PhotoCaptureFrame photoCaptureFrame)
     {
-        // Copy the raw image data into the target texture
+        // Copy the raw image data into our target texture
         photoCaptureFrame.UploadImageDataToTexture(targetTexture);
 
-        // Create a GameObject to which the texture can be applied
+        // Create a gameobject that we can apply our texture to
         GameObject quad = GameObject.CreatePrimitive(PrimitiveType.Quad);
         Renderer quadRenderer = quad.GetComponent<Renderer>() as Renderer;
-        quadRenderer.material = new Material(Shader.Find("Custom/Unlit/UnlitTexture"));
+        quadRenderer.material = new Material(Shader.Find("Unlit/Texture"));
 
         quad.transform.parent = this.transform;
-        quad.transform.localPosition = new Vector3(0.0f, 0.0f, 3.0f);
-
+        quad.transform.localPosition = new Vector3(0.0f, offset, 3.0f);
+        offset += 3.0f;
         quadRenderer.material.SetTexture("_MainTex", targetTexture);
 
-        // Deactivate the camera
+        // Deactivate our camera
         photoCaptureObject.StopPhotoModeAsync(OnStoppedPhotoMode);
     }
 
     void OnStoppedPhotoMode(PhotoCapture.PhotoCaptureResult result)
     {
-        // Shutdown the photo capture resource
+        // Shutdown our photo capture resource
         photoCaptureObject.Dispose();
         photoCaptureObject = null;
     }
