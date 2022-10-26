9006ef4146e9665d2c2759fc86a5ee0e27b399be
Adam Nixon <d.adam.nixon@gmail.com>
2022-10-26 11:58:54 -0500

    Cube/Sphere color selection algorithm

--- Assets/HoloPic.cs
+++ Assets/HoloPic.cs
@@ -9,17 +9,18 @@
 		private Texture2D baseLayer, stripedLayer;
 		private DisplayEnum displayLayers;
 
-		public Holopic(Texture2D original, Color32 selectedColor, int fuzz)
+		public Holopic(Texture2D original, int[] selectedColor, int fuzz)
 		{
 			this.baseLayer = original;
 			this.stripedLayer = processImage(original, selectedColor, fuzz);
 			this.displayLayers = DisplayEnum.BOTH;
 		}
 
-		private Texture2D processImage(Texture2D original, Color32 selectedColor, int fuzz)
+		private Texture2D processImage(Texture2D original, int[] selectedColor, int fuzz)
 		{
-			Color32[] baseImageColors = original.GetPixels32();
+            Color32[] baseImageColors = original.GetPixels32();
 			Texture2D stripes = new Texture2D(2, 2);
+			double dist;
 			if (File.Exists(Application.dataPath + "/stripedHighlights_600x480.png"))
 			{
 				byte[] stripeFile = File.ReadAllBytes(Application.dataPath + "/stripedHighlights_600x480.png");
@@ -33,14 +34,26 @@
 			Debug.Log(baseImageColors.Length);
 			for ( int i = 0; i < stripedLayerArray.Length; i++ )
 			{
-				if (baseImageColors[i].b > 20)
-				stripedLayerArray[i].a = 0;
-			}	
+				dist = ColorDistance(baseImageColors[i], selectedColor);
+
+				if (dist < 100)
+				{
+					stripedLayerArray[i].a = 255;
+					Debug.Log("Color: R" + baseImageColors[i].r + "' G" + baseImageColors[i].g + "' B" + baseImageColors[i].b);
+					Debug.Log(dist);
+				}
+				else if (dist < 100 + fuzz) { stripedLayerArray[i].a = 126; }
+				else { stripedLayerArray[i].a = 0; }
+			}
 			stripes.SetPixels32(stripedLayerArray);
 			stripes.Apply();
 			return stripes;
 		}
 
+		private double ColorDistance(Color32 currentPix, int[] target)
+		{
+            return Math.Sqrt(Math.Pow(currentPix[0] - target[0], 2) + Math.Pow(currentPix[1] - target[1], 2) + Math.Pow(currentPix[2] - target[2], 2));
+		}
 		public void setDisplay(DisplayEnum displaySetting)
 		{
 			this.displayLayers = displaySetting;
