3697d1e7e517b3fb18d734c50040336dad2ecd23
HonestPhoenix <113068231+Zburgess1@users.noreply.github.com>
2022-10-26 12:23:28 -0500

    Modified HoloCamera for FF

--- Assets/HoloCamera.cs
+++ Assets/HoloCamera.cs
@@ -13,9 +13,9 @@ public class HoloCamera : MonoBehaviour
 
     Resolution cameraResolution;
     GameObject quad1 = null;
-    GameObject quad2 = null;
+    //GameObject quad2 = null;
     Renderer quad1Renderer = null;
-    Renderer quad2Renderer = null;
+    //Renderer quad2Renderer = null;
     bool isCapture = false;
     bool done = true;
     // Use this for initialization
@@ -30,15 +30,15 @@ public class HoloCamera : MonoBehaviour
 
         float scale = 0.001f;
         quad1 = GameObject.CreatePrimitive(PrimitiveType.Quad);
-        quad2 = GameObject.CreatePrimitive(PrimitiveType.Quad);
+        //quad2 = GameObject.CreatePrimitive(PrimitiveType.Quad);
 
         quad1.transform.parent = this.transform;
-        quad1.transform.localPosition = new Vector3(0.0f, 0.0f, 3.0f);
+        quad1.transform.localPosition = new Vector3(0.0f, 0.0f, 1.0f);
         quad1.transform.localScale = new Vector3(cameraResolution.width * scale, cameraResolution.height * scale, 0);
 
-        quad2.transform.parent = this.transform;
-        quad2.transform.localPosition = new Vector3(0.0f, 0.0f, 3.0f);
-        quad2.transform.localScale = new Vector3(cameraResolution.width * scale, cameraResolution.height * scale, 0);
+        //quad2.transform.parent = this.transform;
+        //quad2.transform.localPosition = new Vector3(0.0f, 0.0f, 3.0f);
+        //quad2.transform.localScale = new Vector3(cameraResolution.width * scale, cameraResolution.height * scale, 0);
 
         targetTexture = new Texture2D(cameraResolution.width, cameraResolution.height);
 
@@ -50,8 +50,8 @@ public class HoloCamera : MonoBehaviour
         quad1Renderer = quad1.GetComponent<Renderer>() as Renderer;
         quad1Renderer.material = new Material(Shader.Find("Unlit/Texture"));
 
-        quad2Renderer = quad2.GetComponent<Renderer>() as Renderer;
-        quad2Renderer.material = new Material(Shader.Find("Unlit/Transparent"));
+        //quad2Renderer = quad2.GetComponent<Renderer>() as Renderer;
+        //quad2Renderer.material = new Material(Shader.Find("Unlit/Transparent"));
 
     }
 
@@ -77,9 +77,8 @@ public class HoloCamera : MonoBehaviour
         Holopic hp = new Holopic(targetTexture, Color.white, 15);
 
         // apply our texture to our gameobject
-
-        quad1Renderer.material.SetTexture("_MainTex", hp.GetBaseLayer());
-        quad2Renderer.material.SetTexture("_MainTex", hp.GetStripedLayer());
+        quad1Renderer.material.SetTexture("_MainTex", hp.GetStripedLayer());
+        //quad2Renderer.material.SetTexture("_MainTex", hp.GetStripedLayer());
 
         // Deactivate our camera
         photoCaptureObject.StopPhotoModeAsync(OnStoppedPhotoMode);
